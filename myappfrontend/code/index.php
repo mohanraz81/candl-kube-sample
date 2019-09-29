a5601c787bcf315153e0f2be7c80b9b9838f59db
mohanraz81 <mohanraz@gmail.com>
2019-09-29 12:15:57 +0530

    test
    test

--- myappfrontend/code/index.php
+++ myappfrontend/code/index.php
@@ -1,5 +1,5 @@
 <?php
-echo "<h1> This is the Three Tier Architecture Test</h1>";
+echo "<h1> This is the Three Tier Architecture Test New</h1>";
 echo "<h3> Your Web Running Apache with php on Frontend container </h3>";
 echo "<h3> Your APP Running Test  Apache and PHP providing API services for User information on url http://backend/get_user_details </h2>";
 echo "<h3> Your Database running Mysql/Mariadb holding user information in userdb on users Table </h1>";
